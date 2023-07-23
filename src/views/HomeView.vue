d9cd71b027581696b94af4c1c5626cb9559baac2
Rheanne McIntosh <>
2023-07-23 22:46:35 +0100

    Experiment with the composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/TheWelcome.vue';
+import { ref } from "vue";
+
+let message = ref("Hello, World!)";
+
+setTimeout(() => {
+  message.value = "I Have Been Changed";
+}, 2000);
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message" />
+    </p>
   </main>
 </template>
