3802c8fc95b0ace126622a5da505c0a4d810ff36
Rheanne McIntosh <>
2023-07-24 23:10:40 +0100

    Setup Episode 19

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ContactView.vue')
+    },
+    {
+      path: '/episode-19',
+      name: 'episode-29',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Episode19View.vue')
     }
   ]
 })
---
+++ src/views/Episode19View.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+
+    <p>
+      <button>Click Me</button>
+    </p>
+  </main>
+</template>
