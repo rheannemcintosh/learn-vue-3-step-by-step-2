7bba54c5eff0d03924ab7d80e8f54460cc98ccea
Rheanne McIntosh <>
2023-07-25 21:15:34 +0100

    Save favourite food in local storage

--- src/views/Episode20FoodView.vue
+++ src/views/Episode20FoodView.vue
@@ -1,9 +1,18 @@
 <script setup>
+    import { ref } from "vue";
+
+    let food = ref(localStorage.getItem('food'));
+
+    function write() {
+        localStorage.setItem('food', food.value);
+    }
 </script>
 
 <template>
     <main>
         <p>
+            What is your favourite food?
+            <input type="text" v-model="food" @input="write">
         </p>
     </main>
 </template>
