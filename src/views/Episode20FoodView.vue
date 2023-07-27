667d8f4ff795182b22129795cdb65b3c745fbf86
Rheanne McIntosh <>
2023-07-27 22:36:34 +0100

    Watch for food and age changing

--- src/views/Episode20FoodView.vue
+++ src/views/Episode20FoodView.vue
@@ -1,23 +1,36 @@
 <script setup>
-    import { ref } from "vue";
+    import { ref, watch } from "vue";
 
     let food = ref(localStorage.getItem('food'));
     let age = ref(localStorage.getItem('age'));
 
+    watch(food, (val) => {
+        write('food', val)
+    });
+
+    watch(age, (val) => {
+        write('age', val)
+    });
+
     function write(key, value) {
         localStorage.setItem(key, value);
     }
+
+    setTimeout(( => {
+        food.value = 'changed';
+    }, 2000);
+
 </script>
 
 <template>
     <main>
         <p>
             What is your favourite food?
-            <input type="text" v-model="food" @input="write('food', food)">
+            <input type="text" v-model="food">
         </p>
         <p>
             How old are you?
-            <input type="text" v-model="age" @input="write('age', age)">
+            <input type="text" v-model="age">
         </p>
     </main>
 </template>
