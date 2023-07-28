69c8678002981aa6eb3f2521e153a51a9553b6f4
Rheanne McIntosh <>
2023-07-28 23:47:10 +0100

    Refactor the textarea and tab functions

--- src/views/Episode21View.vue
+++ src/views/Episode21View.vue
@@ -1,29 +1,28 @@
 <script setup>
 
-    textarea.addEventListener("keydown", (e) => {
-        // Tab was pressed
-        if (e.keyCode === 9) (
-            // Get caret position / selection
-            let val = textarea.value,
-                start = textarea.selectionStart,
-                end = textarea.selectionEnd;
+    import { ref } from "vue";
 
-            // Set textarea value to: text before caret + tab + text after caret
-            textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+    function onTabPress() {
+        let textarea = e.target;
 
-            // Put caret at right position again
-            textarea.selectionStart = textarea.selectionEnd = start + 1;
+        // Get caret position / selection
+        let val = textarea.value,
+            start = textarea.selectionStart,
+            end = textarea.selectionEnd;
 
-            e.preventDefault();
-        )
-    })
+        // Set textarea value to: text before caret + tab + text after caret
+        textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+        // Put caret at right position again
+        textarea.selectionStart = textarea.selectionEnd = start + 1;
+    }
 
 </script>
 
 <template>
     <main>
         <form>
-            <textarea style="width: 100%; height: 300px;"></textarea>
+            <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px;">Hi There</textarea>
         </form>
     </main>
 </template>
