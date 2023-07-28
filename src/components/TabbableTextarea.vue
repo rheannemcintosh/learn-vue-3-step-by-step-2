3f9a216a5f3f6a5ab9499322dc43dbc02b0406f1
Rheanne McIntosh <>
2023-07-29 00:07:38 +0100

    Move to a tabbable componenet

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,20 @@
+<script>
+    function onTabPress() {
+        let textarea = e.target;
+
+        // Get caret position / selection
+        let val = textarea.value,
+            start = textarea.selectionStart,
+            end = textarea.selectionEnd;
+
+        // Set textarea value to: text before caret + tab + text after caret
+        textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+        // Put caret at right position again
+        textarea.selectionStart = textarea.selectionEnd = start + 1;
+    }
+</script>
+
+<template>
+    <textarea @keydown.tab.prevent="onTabPress"></textarea>
+</template>
--- src/views/Episode21View.vue
+++ src/views/Episode21View.vue
@@ -1,28 +1,11 @@
 <script setup>
-
-    import { ref } from "vue";
-
-    function onTabPress() {
-        let textarea = e.target;
-
-        // Get caret position / selection
-        let val = textarea.value,
-            start = textarea.selectionStart,
-            end = textarea.selectionEnd;
-
-        // Set textarea value to: text before caret + tab + text after caret
-        textarea.value = val.substring(0, start) + "\t" + val.substring(end);
-
-        // Put caret at right position again
-        textarea.selectionStart = textarea.selectionEnd = start + 1;
-    }
-
+    import TabbableTextarea from "@/components/TabbableTextarea.vue";
 </script>
 
 <template>
     <main>
         <form>
-            <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px;">Hi There</textarea>
+            <TabbableTextarea style="width: 100%; height: 300px;" />
         </form>
     </main>
 </template>
