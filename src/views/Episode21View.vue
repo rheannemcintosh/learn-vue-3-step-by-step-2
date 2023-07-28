b597cfc3b55f11681b0c638d10b255fbf9c22478
Rheanne McIntosh <>
2023-07-29 00:09:02 +0100

    Create a comment tabbable text area

--- src/views/Episode21View.vue
+++ src/views/Episode21View.vue
@@ -1,11 +1,16 @@
 <script setup>
     import TabbableTextarea from "@/components/TabbableTextarea.vue";
+
+    import { ref } from "vue";
+
+
+    let comment = ref('test value');
 </script>
 
 <template>
     <main>
         <form>
-            <TabbableTextarea style="width: 100%; height: 300px;" />
+            <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;" />
         </form>
     </main>
 </template>
