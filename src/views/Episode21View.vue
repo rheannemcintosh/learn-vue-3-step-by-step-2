b36742a6778d70d21c2b5c86482c51c3364a3ff0
Rheanne McIntosh <>
2023-07-28 22:57:08 +0100

    Add example JS for tabbing text in a textarea

--- src/views/Episode21View.vue
+++ src/views/Episode21View.vue
@@ -1,4 +1,23 @@
 <script setup>
+
+    textarea.addEventListener("keydown", (e) => {
+        // Tab was pressed
+        if (e.keyCode === 9) (
+            // Get caret position / selection
+            let val = textarea.value,
+                start = textarea.selectionStart,
+                end = textarea.selectionEnd;
+
+            // Set textarea value to: text before caret + tab + text after caret
+            textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+            // Put caret at right position again
+            textarea.selectionStart = textarea.selectionEnd = start + 1;
+
+            e.preventDefault();
+        )
+    })
+
 </script>
 
 <template>
