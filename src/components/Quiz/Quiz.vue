c2b04fb85af9cf3fa4b97673b718f4baa8366d94
Rheanne McIntosh <>
2023-07-30 16:01:00 +0100

    Remove old code

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -11,14 +11,4 @@
   import QuizHeader from "@/components/Quiz/QuizHeader.vue";
   import QuizQuestion from "@/components/Quiz/QuizQuestion.vue";
   import QuizFooter from "@/components/Quiz/QuizFooter.vue";
-  import { provide, ref } from "vue";
-
-  defineProps({ quiz: Object });
-
-  let name = ref('John Doe');
-
-  provide('name', {
-    name,
-    changeName: () => name.value = 'Changed';
-  });
 </script>
