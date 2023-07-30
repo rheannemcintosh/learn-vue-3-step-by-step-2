293c4f8271e53521cd0d3e37663e4690ed15e0d2
Rheanne McIntosh <>
2023-07-30 16:39:01 +0100

    Implement basic counter

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,10 +1,15 @@
 <script setup>
+    import { counter } from "@/stores/counterStore";
 </script>
 
 <template>
   <div>
-    <p>
+    <h1>
       Counter View
+    </h1>
+    <p>
+      {{ counter.count }}
     </p>
+    <button @click="counter.count++">Increment</button>
   </div>
 </template>
