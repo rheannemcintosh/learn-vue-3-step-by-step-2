ccb0b6307f09bce8596d12a10657a51ec01d6392
Rheanne McIntosh <>
2023-07-30 16:15:56 +0100

    Create quiz header

---
+++ src/components/Quiz/QuizHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h1>Quiz Header</h1>
+
+        <p>
+            Welcome to your first quiy. How do you think you'll do?
+        </p>
+    </header>
+</template>
+
+<script setup>
+</script>
